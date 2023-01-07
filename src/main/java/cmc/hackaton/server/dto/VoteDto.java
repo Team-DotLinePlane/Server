f35ac094ba426ca880fb632cf0cb0fc60d8a13ba
Wo-ogie <siwall0105@gmail.com>
2023-01-08 03:35:01 +0900

    feat: VoteDto

---
+++ src/main/java/cmc/hackaton/server/dto/VoteDto.java
@@ -0,0 +1,46 @@
+package cmc.hackaton.server.dto;
+
+import cmc.hackaton.server.entity.Member;
+import cmc.hackaton.server.entity.Team;
+import cmc.hackaton.server.entity.constant.FoodCategory;
+import cmc.hackaton.server.entity.Vote;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class VoteDto {
+
+    private Team team;
+    private Member voteLeader;
+    private FoodCategory selectedFoodCategory;
+    private LocalDateTime expiredTime;
+    private Boolean isCompleted;
+
+    public static VoteDto of(Team team, Member voteLeader, FoodCategory selectedFoodCategory, LocalDateTime expiredTime, Boolean isCompleted) {
+        return new VoteDto(team, voteLeader, selectedFoodCategory, expiredTime, isCompleted);
+    }
+
+    public static VoteDto from(Vote vote) {
+        return new VoteDto(
+                vote.getTeam(),
+                vote.getVoteLeader(),
+                vote.getSelectedFoodCategory(),
+                vote.getExpiredTime(),
+                vote.getIsCompleted()
+        );
+    }
+
+    public Vote toEntity() {
+        return Vote.builder()
+                .team(team)
+                .voteLeader(voteLeader)
+                .selectedFoodCategory(selectedFoodCategory)
+                .expiredTime(expiredTime)
+                .isCompleted(isCompleted)
+                .build();
+    }
+}
