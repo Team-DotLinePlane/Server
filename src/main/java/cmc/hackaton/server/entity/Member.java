d5d87251487e512aac7ca622ba1f240bae898e73
Wo-ogie <siwall0105@gmail.com>
2023-01-08 01:18:15 +0900

    member table column name 변경
    OAuth를 하지 않을 것이므로 `identifier`보다는 `token`이라는 naming이 더 적합해보여 수정함

--- src/main/java/cmc/hackaton/server/entity/Member.java
+++ src/main/java/cmc/hackaton/server/entity/Member.java
@@ -19,14 +19,14 @@ public class Member {
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private String identifier;  // 익명 유저의 UUID
+    private String token;  // 익명 유저의 UUID
 
     @Column(nullable = false)
     private String nickname;
 
     @Builder
-    private Member(String identifier, String nickname) {
-        this.identifier = identifier;
+    private Member(String token, String nickname) {
+        this.token = token;
         this.nickname = nickname;
     }
 }
