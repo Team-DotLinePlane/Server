8bc0586c239c92eb6a3e8972b1e9b48cbbfbd447
BEMELON <hgdkim2@naver.com>
2023-01-07 18:43:33 +0900

    style: 누락된 속성 추가

--- src/main/java/cmc/hackaton/server/entity/GroupMembers.java
+++ src/main/java/cmc/hackaton/server/entity/GroupMembers.java
@@ -1,6 +1,5 @@
 package cmc.hackaton.server.entity;
 
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,7 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
