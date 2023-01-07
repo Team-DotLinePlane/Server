8385ba01acc34aa6335f0836e1422285312567c2
Wo-ogie <siwall0105@gmail.com>
2023-01-07 18:58:48 +0900

    feature: 음식 종류 enum class 정의
    음식 종류 카테고리로 사용할 enum class를 정의했다.

---
+++ src/main/java/cmc/hackaton/server/entity/constant/FoodCategory.java
@@ -0,0 +1,20 @@
+package cmc.hackaton.server.entity.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum FoodCategory {
+
+    JOKBAL_BOSSAM,      // 족발, 보쌈
+    JJIM_TANG_JJIGAE,   // 찜, 탕, 찌개
+    JAPANESE,           // 일식 (돈까스, 회, 일식)
+    PIZZA,              // 피자
+    MEAT,               // 고기, 구이
+    WESTERN,            // 양식
+    CHICKEN,            // 치킨
+    CHINESE,            // 중식
+    ASIAN,              // 아시안
+    BAEKBAN_JUK_NOODLE, // 백반, 죽, 국수
+    BUNSIK,             // 분식
+    FASTFOOD;           // 패스트푸드
+}
